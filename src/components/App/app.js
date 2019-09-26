a7c97448d985af34f142fe17364a9f18fb39538a
Vlad Ovchynnyk <ovchnkvlad@gmail.com>
2019-09-26 19:21:56 +0300

    end todo app

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -17,7 +17,9 @@ export default class App extends Component {
       this.createTodoItem("Drink Coffee"),
       this.createTodoItem("Make Awesome App"),
       this.createTodoItem("Have a lunch")
-    ] 
+    ],
+    term: '',
+    filter: 'all' //active, all, done
   }
 
   createTodoItem(label){
@@ -84,9 +86,40 @@ export default class App extends Component {
       };
     });
   };
+  search = (items, term) => {
+    if(term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+    });
+  };
+  filter = (items, filter) => {
+    switch(filter){
+      case 'all': 
+         return items;
+      case 'active': 
+        return items.filter((item) => !item.done);
+      case 'done':
+        return items.filter((item) => item.done);
+      default: 
+        return items;
+    }
+  };
+  onSearchChange = (term) => {
+    this.setState({term});
+  };
+  onFilterChange = (filter) => {
+    this.setState({filter});
+
+  }
  render() {
 
-  const { todoData } = this.state;
+  const { todoData, term, filter } = this.state;
+
+  const visibleItems = this.filter(
+    this.search(todoData, term),filter
+    );
 
   const doneCount = todoData.filter((el) => el.done).length;
 
@@ -96,11 +129,16 @@ export default class App extends Component {
     <div className="todo-app">
     <AppHeader toDo={todoCount} done={doneCount} />
     <div className="top-panel d-flex">
-      <SearchPanel />
-      <ItemStatusFilter />
+      <SearchPanel 
+      onSearchChange = {this.onSearchChange}
+      />
+      <ItemStatusFilter 
+        filter = {filter}
+        onFilterChange = {this.onFilterChange}
+      />
     </div>
 
-    <TodoList todos={todoData}
+    <TodoList todos={visibleItems}
       onDeleted = {this.deleteItem}
       onTogleImportant = {this.onTogleImportant}
       onTogleDone = {this.onTogleDone}
